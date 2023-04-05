e40e7b75343e82c9b58ab94a33de13d60eb081db
LI Tian <li_tian@singaporeair.com.sg>
2023-04-05 13:10:47 +0800

    step4: make celery work in container

--- example/api.py
+++ example/api.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
-from fastapi.security import OAuth2PasswordBearer
+from example.celery_worker import do_this_with_worker
 
 api = FastAPI()
 
 
-@api.get("/test")
+@api.get("/test_fastapi")
 async def test_fastapi():
     return {"message": "[ok] FastAPI is working"}
+
+
+@api.get('/test_celery')
+async def test_celery():
+    task = do_this_with_worker.delay()
+    return JSONResponse({"Result": task.get()})
